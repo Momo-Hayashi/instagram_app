38e565b497219a6d7dbe2206c7ea3c1ee6e5c3b9
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-14 16:57:44 +0900

    ユーザー登録機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :posts do
     collection do
       post :confirm
     end
   end
+  root 'posts#index'
 end
