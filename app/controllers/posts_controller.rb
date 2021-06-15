8a56270a0d585e53dca7fd95147ff389cf66927e
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-15 17:02:31 +0900

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,7 @@ class PostsController < ApplicationController
       render :new
     else
       if @post.save
+        PostingMailer.confirmation_mail(@confirmation).deliver
         redirect_to posts_path, notice: 'Successfully posted!'
       else
         render :new
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: '14hb108e@al.rikkyo.ac.jp'
   layout 'mailer'
 end
